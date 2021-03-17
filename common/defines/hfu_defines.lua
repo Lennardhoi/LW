35f49f6e6cfd7c3d6c73be54c3c28adf35d29489
Lennardhoi <55025658+Lennardhoi@users.noreply.github.com>
2021-03-17 17:46:27 +0100

    update
    -fixed resource rights in swpa
    -germany and ita start with radio and radar 1
    radar base cost from 2k to 1k per level set to 500
    -tac bombers are around 20% slower
    -base spotting from occupied territory increased from 10% to 20%
    -atlantikwall spirit and mission are 60days now
    -passive defense from minors decreased from 5 to 3
    -extra ahead of time updates every half year instead of every year. 43 techs had no penalty for some reason but now they do
    -soviets are limited to 800 convoys excess goes to uk

--- common/defines/hfu_defines.lua
+++ common/defines/hfu_defines.lua
@@ -40,6 +40,7 @@ NDefines.NTrade.BASE_LAND_TRADE_RANGE = 350
 NDefines.NAir.AIR_WING_MAX_SIZE = 1600                            -- this can be halved 4 times into 100 stacks (very convinient), up from 1600 | 
 NDefines.NAir.AIR_DEPLOYMENT_DAYS = 0                              -- Down from 3 | Makes AC player much more responsive
 NDefines.NAir.AIR_WING_BOMB_DAMAGE_FACTOR = 1.4 --controlls strat bombing damage was too strong on 5 but might need to be high to actually damage something
+NDefines.NAir.DETECT_CHANCE_FROM_OCCUPATION = 0.2
 ----------------------------------------------------------------------------------------------------------------------------
 NDefines.NDiplomacy.NUM_DAYS_TO_ENABLE_KICKING_NEW_MEMBERS_OF_FACTION = 14	-- down from 90 | Number of days before being able to kick a new member of faction 
 NDefines.NDiplomacy.NUM_DAYS_TO_ENABLE_REINVITE_KICKED_NATIONS = 14		-- down from 90 | Number of days before being able to re invite a kicked 
