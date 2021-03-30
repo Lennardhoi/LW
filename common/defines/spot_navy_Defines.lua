67a7341ae92b90ec28318e4ee439e3405ea8eb72
Lennardhoi <55025658+Lennardhoi@users.noreply.github.com>
2021-03-30 15:24:55 +0200

    update
    -France gets 10% more factory output pre cap
    -germany gets africa hp buff after poland
    -paris city tiles have minus 50% org regain until fall of france
    -paris only counts once for the cap score
    -disruption factor 5-->6
    -escort factor 5-->4
    -max ship aa damage reduction 0.8 -->0.7
    -21 ship aa is still needed for max reduction
    -german heavy tank spawnins have armor upgrades
    -heavy fighters have no range upgrades anymore and are preupgraded on range so the upgraded fighter variants are no longer needed
    -shbb guns dont require chromium anymore and have 60 instead of 50 piercing
    -transport planes have 5 more  air defence
    -soviet relocation slowed down 4 -->5 days
    -barb buff gives additional -7.5% defense and +5% breakthrough against soviet union

--- common/defines/spot_navy_Defines.lua
+++ common/defines/spot_navy_Defines.lua
@@ -15,8 +15,8 @@ NDefines.NNavy.SCREEN_RATIO_FOR_FULL_SCREENING_FOR_CAPITALS = 2.5
 
 NDefines.NNavy.ANTI_AIR_POW_ON_INCOMING_AIR_DAMAGE = 0.8 --- (0.2 -> 0.8)
 NDefines.NNavy.SHIP_TO_FLEET_ANTI_AIR_RATIO = 0.01 --- (0.2 -> 0.01)
-NDefines.NNavy.ANTI_AIR_MULT_ON_INCOMING_AIR_DAMAGE = 0.07 --- (0.15 -> 0.07)
-NDefines.NNavy.MAX_ANTI_AIR_REDUCTION_EFFECT_ON_INCOMING_AIR_DAMAGE = 0.8 --- (0.5 -> 0.8)
+NDefines.NNavy.ANTI_AIR_MULT_ON_INCOMING_AIR_DAMAGE = 0.065 --- (0.15 -> 0.07)
+NDefines.NNavy.MAX_ANTI_AIR_REDUCTION_EFFECT_ON_INCOMING_AIR_DAMAGE = 0.7 --- (0.5 -> 0.8)
 
 NDefines.NNavy.HIGHER_SHIP_RATIO_POSITIONING_PENALTY_FACTOR	= 0.3 -- if one side has more ships than the other, that side will get this penalty for each +100% ship ratio it has
 NDefines.NNavy.MAX_POSITIONING_PENALTY_FROM_HIGHER_SHIP_RATIO = 0.7  -- maximum penalty to get from larger fleets
